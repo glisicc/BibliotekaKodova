540019c5c2263383d56314358c51005a6d5a2447
[Stefan Glisic] <glisicc.s@gmail.com>
2022-06-14 08:19:59 +0200

    push

--- REACT/Section18-REDUX/src/store/index.js
+++ REACT/Section18-REDUX/src/store/index.js
@@ -1,7 +1,19 @@
-import {createStore} from 'redux'
+import { createStore } from "redux";
 
-const counterReducer = (state = {counter: 0}, action) => {
-    if(action.type === '')
-}
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
 
-const store = createStore()
+const store = createStore(counterReducer);
+
+export default store;
